f6e78e420ca390e84523660c2e784b366cff0d56
bayupradana7585 <bayupradana7585@gmail.com>
2025-08-20 22:13:07 +0700

    Lingkaran

---
+++ Luas Lingkaran.php
@@ -0,0 +1,10 @@
+<?php
+// Input nilai
+$jari_jari = 7;
+
+// Hitung luas
+$hasil = 3.14159 * $jari_jari * $jari_jari;
+
+// Tampilkan hasil
+echo "Luas Lingkaran dengan jari-jari $jari_jari adalah $hasil";
+?>
