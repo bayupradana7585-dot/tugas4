95a5bcaa14e07b85ceddc6bcd1959767518102c6
bayupradana7585 <bayupradana7585@gmail.com>
2025-08-20 20:11:07 +0700

    volume-balo.php

---
+++ volume-balok.php
@@ -0,0 +1,13 @@
+<?php
+// Input nilai
+$panjang = 10;
+$lebar = 5;
+$tinggi = 3;
+
+// Hitung volume
+$hasil = $panjang * $lebar * $tinggi;
+
+// Tampilkan hasil
+echo "Volume Balok dengan panjang $panjang, lebar $lebar dan tinggi $tinggi adalah $hasil";
+?>
+
