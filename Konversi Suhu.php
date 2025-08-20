bdef89a056024720fdc3ad924f6336b3d3741c9a
bayupradana7585 <bayupradana7585@gmail.com>
2025-08-20 22:15:45 +0700

    suhu

---
+++ Konversi Suhu.php
@@ -0,0 +1,10 @@
+<?php
+// Input nilai
+$celcius = 30;
+
+// Konversi suhu
+$fahrenheit = ($celcius * 9/5) + 32;
+
+// Tampilkan hasil
+echo "$celcius derajat Celcius sama dengan $fahrenheit derajat Fahrenheit";
+?>
